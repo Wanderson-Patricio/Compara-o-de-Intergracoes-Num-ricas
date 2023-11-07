2d868abd2a7e1eabc94150bcd70440cc8c80ad06
Wanderson-Patricio <118014817+Wanderson-Patricio@users.noreply.github.com>
2023-11-07 19:14:44 -0300

    Update RiemannIntegration.py

--- RiemannIntegration.py
+++ RiemannIntegration.py
@@ -21,3 +21,28 @@ for i in range(5):
     n = 10**i
     soma = sum(0, 1, n, f)
     print(f'n = {n} : soma = {soma} , erro = {abs(0.5 - soma)/(0.005)}')
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+fig, ax = plt.subplots()
+
+for i in range(5):
+    n = 10**i
+    x = []
+    y = []
+    deltaX = dx(0, 1, n)
+    for i in range(1, n+1):
+        x.append(i*deltaX)
+        y.append(sum(0,i*deltaX,n,f))
+
+    ax.plot(x, y, label = f'n = {n}', ls = '--')
+
+
+x = np.linspace(0,1, 100)
+y = [(i**2)/2 for i in x]
+ax.plot(x, y, label = 'valor real', color = 'black')
+
+ax.set(ylim = (0,0.6))
+ax.legend()
+plt.show()
